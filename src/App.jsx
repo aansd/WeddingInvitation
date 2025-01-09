44604d32f34cd6549592fd054f63781ce79d8878
aansd <septiyan63@gmail.com>
2025-01-09 21:02:14 +0700

    update 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import WeddingLandingPage from "./assets/Pages/WeddingLandingPage/WeddingLandingPage"
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-      <ToastContainer/>
-      <Route path="/" element={<WeddingLandingPage />} />
-    </Routes>
-  </BrowserRouter>
+  <div>
+    
+    <ToastContainer/>
+     <WeddingLandingPage />
+   
+  </div>
   )
 }
 
